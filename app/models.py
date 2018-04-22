2a68695bd0a9d128c6ab9134e7b138ee7c9963ac
garvinipkiss <garvinipkiss@gmail.com>
2018-04-22 22:25:03 +0300

    added passwordhasher

--- app/models.py
+++ app/models.py
@@ -18,3 +18,17 @@ class User(UserMixin, db.Model):
   email = db.Column(db.String(255),unique = True,index = True)
   role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
   password_hash =db.Column(db.String(255))
+
+   pass_secure  = db.Column(db.String(255))
+
+        @property
+        def password(self):
+            raise AttributeError('You cannot read the password attribute')
+
+        @password.setter
+        def password(self, password):
+            self.pass_secure = generate_password_hash(password)
+
+
+        def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
