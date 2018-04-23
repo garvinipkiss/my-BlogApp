f607c6a2d5b4fce8338ba350d04b9b4eb6f5d3c0
garvinipkiss <garvinipkiss@gmail.com>
2018-04-23 18:41:39 +0300

    comments.html

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,68 @@
+from flask import render_template, request, redirect, url_for, abort
 from . import main
-from flask import render_template
-
+from flask_login import login_required,current_user
+from ..models import User,Blog,Comment,Role
+from .forms import BlogForm,CommentForm,DeleteBlog
+from .. import db
+import markdown2
 
 
 @main.route('/')
 def index():
 
   title ="Garvin's Manenos"
-  return render_template('index.html', title = title)
+  return render_template('index.html', title = title,blogs=blogs)
+
+@main.route('/blog/<int:id>')
+def blog(id):
+  blogs = Blog.query.get(id)
+  comments = Comment.get_comments(id)
+  form = CommentForm()
+  title ="Blogs"
+
+  return render_template('blog.html', title =title, blogs=blogs,comments=comments,comment_form= form)
+
+
+
+@main.route('/blog/<int:id>',methods=["GET","POST"])
+def new_comment(id):
+  blog = Blog.query.filter_by(id=id).first()
+
+  form = CommentForm()
+
+  if form.validate_on_submit():
+    content = form.content.data
+
+    new_comment = Comment(content = content, blog_id = blog.id)
+    new_comment.save_comment()
+
+    return redirect(url_for('.blog', id = blog.id))
+
+  return render_template('blog.html', comment_form =form)
+
+@main.route('/blog/comment/<int:id>')
+def single_comment(id):
+
+  comment = Comment.query.get(id)
+
+  if comment is None:
+    abort(404)
+
+  format_comment = markdown2.markdown(comment.content,extras=["code-friendly", "fenced-code-blocks"])
+  return render_template('comment.html', comment= comment, format_comment= format_comment)
+
+@main.route('/blog/comment/delete/<int:id>')
+def delete_comment(id):
+
+  comment = Comment.query.get(id)
+  comment.delete_comments(id)
+
+  return redirect(url_for('.index'))
+
+@main.route('/blog/delete/<int:id>')
+def delete_blog(id):
+
+  blog = Blog.query.get(id)
+  blog.delete_blog(id)
+
+  return redirect(url_for('.index'))
--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ from . import  login_manager
 
 @login_manager.user_loader
 def load_user(user_id):
-  return User.query.get(int(user_id))
+    return User.query.get(int(user_id))
 
 
 
@@ -19,16 +19,16 @@ class User(UserMixin, db.Model):
   role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
   password_hash =db.Column(db.String(255))
 
-   pass_secure  = db.Column(db.String(255))
+  pass_secure  = db.Column(db.String(255))
 
-        @property
-        def password(self):
-            raise AttributeError('You cannot read the password attribute')
+@property
+def password(self):
+    raise AttributeError('You cannot read the password attribute')
 
-        @password.setter
-        def password(self, password):
-            self.pass_secure = generate_password_hash(password)
+@password.setter
+def password(self, password):
+    self.pass_secure = generate_password_hash(password)
 
 
-        def verify_password(self,password):
-            return check_password_hash(self.pass_secure,password)
+def verify_password(self,password):
+    return check_password_hash(self.pass_secure,password)
