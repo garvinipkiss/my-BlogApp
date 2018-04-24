b8fef51dabc2c7fc653325f83bf113d79a07db5d
garvinipkiss <garvinipkiss@gmail.com>
2018-04-24 11:35:56 +0300

    Initial Commit

--- config.py
+++ config.py
@@ -1,27 +1,3 @@
-import os
-
-class Config:
-	SECRET_KEY=os.environ.get('SECRET_KEY')
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	# simple mde  configurations
-	SIMPLEMDE_JS_IIFE = True
-	SIMPLEMDE_USE_CDN = True
-
-
-
-class ProdConfig(Config):
-  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
-
-
-class DevConfig(Config):
-	DEBUG =True
-	SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://garvin:0000@localhost/blogapp'
-
-config_options={
-	'development':DevConfig,
-	'production':ProdConfig,
-}
 class Config:
     pass
 
--- manage.py
+++ manage.py
@@ -1,28 +1,6 @@
 
 from flask_script import Manager,Server
 from app import create_app,db
-from flask_migrate import Migrate, MigrateCommand
-
-app = create_app('development')
-
-manager = Manager(app)
-migrate = Migrate(app,db)
-manager.add_command('db',MigrateCommand)
-
-manager.add_command('server', Server)
-
-@manager.command
-def test():
-    """Run the unit tests."""
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
-
-if __name__ == '__main__':
-    manager.run()
-
-from flask_script import Manager,Server
-from app import create_app,db
 
 app = create_app('default')
 
